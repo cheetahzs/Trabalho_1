4e8fa7d973e2caab9a3fe102b1ded5b2f71ab223
cheetahzs <ldbemri@gmail.com>
2025-05-26 11:47:34 -0300

    feat: metodo vencedor

--- limite/tela_voto.py
+++ limite/tela_voto.py
@@ -5,6 +5,7 @@ class TelaVoto:
         print("2 - Alterar Voto")
         print("3 - Listar Voto")
         print("4 - Excluir Voto")
+        print("5 - Apurar Vencedores")
         print("0 - Retornar")
 
         opcao = int(input("Escolha a opcao: "))
